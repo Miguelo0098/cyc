4149ab2011e4bcc5b27f17a6b3cd4bf35de5d311
Miguelo0098 <i62rarum@uco.es>
2019-11-16 14:43:21 +0100

    añadida primera funcion practica 6

---
+++ p6/genero_clave.m
@@ -0,0 +1,11 @@
+function [cpublica, cprivada] = genero_clave()
+%genero_clave - Description
+%
+% Syntax: [cpublica, cprivada] = genero_clave()
+%
+% Programa que genera las claves públicas y privadas que 
+% se necesitan para el cifrado con RSA
+
+
+
+end
