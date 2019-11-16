76fb7cf21ce7ddbf6a40812c44df31c10545c92d
miguelo0098 <mugui098@gmail.com>
2019-11-16 16:36:17 +0100

    añadida parte de la generación de claves

--- p6/genero_clave.m
+++ p6/genero_clave.m
@@ -6,6 +6,28 @@ function [cpublica, cprivada] = genero_clave()
 % Programa que genera las claves públicas y privadas que 
 % se necesitan para el cifrado con RSA
 
+p = input('Introduzca el valor del primo p: ');
+q = input('Introduzca el valor del primo q: ');
+disp(' ');
+
+if any(mod([p g],1) ~= 0)
+    disp('ERROR: No se puede realizar el intercambio, fin del programa');
+    return;
+end
+
+n = p * q;
+
+disp(['n, que formará parte de las claves es: n = ' int2str(n)]);
+disp(' ');
+
+fiden = (p - 1) * (q - 1);
+disp(['Estamos buscando e, que formara parte de la clave privada y que debe cumplir gcd(e,fiden) = gcd(e,p∙q) = gcd(e,' int2str(fiden) ') = 1,']);
+disp(' ');
+
+e = 65537;
+
+if e < fiden && gcd(e, fiden) == 1
+    
 
 
 end
