9a9ac1cd8f361de1f3cf1b16227beb6cebbe3aaa
miguelo0098 <mugui098@gmail.com>
2019-11-17 15:03:07 +0100

    añadida funcion letra2numeros solo descripcion

---
+++ p6/letra2numeros.m
@@ -0,0 +1,16 @@
+function doble = letra2numeros(texto)
+%letra2numeros - Description
+%
+% Syntax: doble = letra2numeros(texto)
+%
+% Función que asocia su correspondiente valor 
+% de Z27 con dos digitos: a:00, b:01, ..., z:26.
+%
+% Entradas: texto: el texto llano
+%
+% Salida: doble: cadena numérica formada por los números 
+%                asociados a cada letra del texto.
+
+
+
+end
