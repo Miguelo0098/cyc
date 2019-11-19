f8a6c0a30b3c38f00041c1192935d9dd35f5ebf0
Miguelo0098 <i62rarum@uco.es>
2019-11-19 14:37:03 +0100

    ejercicio 9 listo (esqueleto base)

--- p6/autenticacion.m
+++ p6/autenticacion.m
@@ -24,4 +24,54 @@ disp(['Clave privada de A: (na,da) = (' int2str(cpriva(1)) ',' int2str(cpriva(2)
 disp(['Clave pública de B: (na,eb) = (' int2str(cpublb(1)) ',' int2str(cpublb(2)) ')']);
 disp(['Clave privada de B: (na,db) = (' int2str(cprivb(1)) ',' int2str(cprivb(2)) ')']);
 
+na = cpubla(1);
+na = 27371551;
+ea = cpubla(2);
+ea = 13;
+da = cpriva(2);
+da = 18941533;
+
+nb = cpublb(1);
+nb = 492859;
+eb = cpublb(2);
+eb = 179;
+db = cprivb(2);
+db = 422459;
+
+disp(' ');
+
+disp('A quiere enviar, junto con su firma, lo siquiente:')
+
+mensaje = 'el programa funciona byalma';
+mensajefirma = 'byalma';
+
+disp(['mensaje = ' mensaje]);
+disp(' ');
+
+cif_mens = cifro_rsa(eb, nb, mensaje);
+
+cif_firma_da = cifro_rsa(da, na, mensajefirma)
+
+cif_firma_da_eb = cifro_rsa_num(eb,nb,cif_firma_da)
+
+disp('Los dos criptogramas que envia A a B son:');
+disp(['cif_mens = ' int2str(cif_mens)]);
+disp(['cif_firma_da_eb = ' int2str(cif_firma_da_eb)]);
+
+disp(' ');
+
+disp('B comienza el descifrado');
+disp(' ');
+
+mensaje = descifro_rsa(db,nb, cif_mens);
+cif_firma_da = descifro_rsa_num(db,nb, cif_firma_da_eb)
+firma = descifro_rsa(ea,na,cif_firma_da)
+
+disp('El mensaje con la firma que recibe es: ');
+disp(['mensaje = ' mensaje]);
+disp(' ');
+disp('B obtiene la firma y comprueba su autentificacion');
+disp(['firma = ' firma]);
+
+
 end
---
+++ p6/bloque_translator.m
@@ -0,0 +1,15 @@
+function newbloque = bloque_translator(oldn,newn, oldbloque)
+%bloque_translator - Description
+%
+% Syntax: newbloque = bloque_translator(oldn,newn,oldbloque)
+%
+% Funcion que traduce el tamaño de un bloque a otro nuevo en 
+% funcion de la n nueva con la que se vaya a cifrar en RSA
+%
+% Entradas: oldn: antigua n que determina el tamaño de oldbloque
+%           newn: nueva n que determina el tamaño de newbloque
+%           oldbloque: antiguo bloque a transformar
+%
+% Salida: newbloque: nuevo bloque listo para cifrar
+    
+end
